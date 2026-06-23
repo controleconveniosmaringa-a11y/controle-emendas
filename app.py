ecbe7093c687fba0bf7b147735bc1504862afcc2
controleconveniosmaringa-a11y <controleconveniosmaringa@gmail.com>
2026-06-23 13:18:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ try:
         # 1. 🎯 ABA POR FONTE
        
      with tab_ativa:
-         fonte_sel = st.selectbox("🎯 Selecione a Fonte Orçamentária para detalhar:", options=fontes, index=0, key="selectbox_fonte_exclusiva_aba")
+     fonte_sel = st.selectbox("🎯 Selecione a Fonte Orçamentária para detalhar:", options=fontes, index=0, key="selectbox_fonte_exclusiva_aba")
             
             if fonte_sel:
                 # O restante do código abaixo deve ter 16 espaços de recuo
